fa00d02d0b8ee49d485d8cf7e46b623bf9cc5368
Hasan-Saju <hasansaju11@gmail.com>
2021-08-04 02:57:43 +0600

    MO registration form connected with MongoDB

--- controllers/mathOlympiad.controller.js
+++ controllers/mathOlympiad.controller.js
@@ -12,7 +12,53 @@ const postMO = (req, res) => {
   console.log(email);
   console.log(institution);
   console.log(tshirt);
-  res.render("math-olympiad/register.ejs");
+
+  let registrationFee = 0;
+  if (category == "School") {
+    registrationFee = 250;
+  } else if (category == "College") {
+    registrationFee = 400;
+  } else {
+    registrationFee = 500;
+  }
+
+  const total = registrationFee;
+  const paid = 0;
+  const selected = false;
+
+  let error = "";
+
+  MathOlympiad.findOne({ name: name, contact: contact }).then((participant) => {
+    if (participant) {
+      error = "Participant with this name and contact number already exists!";
+      console.log(error);
+      res.redirect("register");
+    } else {
+      const participant = new MathOlympiad({
+        name,
+        category,
+        contact,
+        email,
+        institution,
+        paid,
+        total,
+        selected,
+        tshirt,
+      });
+      participant
+        .save()
+        .then(() => {
+          error = "Participant has been registered successfully!";
+          console.log(error);
+          res.redirect("register");
+        })
+        .catch(() => {
+          error = "An unexpected error occured while registering participant";
+          console.log(error);
+          res.redirect("register");
+        });
+    }
+  });
 };
 
 const getMOList = (req, res) => {
